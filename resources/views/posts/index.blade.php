9c7049ed3a50d1f53255a92fbd7214262f581146
rene villegas <duarapcsis@gmail.com>
2018-10-13 01:23:48 -0300

    creacion de vistas

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+       	
+           <div class="col-md-12">
+        	
+             <h2>Title</h2>
+             <p>postes 5 hrs ago </p>
+
+           </div>
+
+        </div>
+
+        <div class="row">
+       	
+           <div class="col-md-12">
+        	
+             <h2>Title</h2>
+             <p>postes 5 hrs ago </p>
+
+           </div>
+
+        </div>
+
+        <div class="row">
+       	
+           <div class="col-md-12">
+        	
+             <h2>Title</h2>
+             <p>postes 5 hrs ago </p>
+
+           </div>
+
+        </div>
+
+      </div>
+
+
+@endsection
+
+       
+
+
+
+
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="col-md-12">
+        	
+             <h2>Title</h2>
+             <p>postes 5 hrs ago </p>
+
+           </div>
+
+
+@endsection
